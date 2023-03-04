2ab081e9f12ac95a23d252e5770c0464091f794d
JBONES <jalilbones10@gmail.com>
2023-03-04 15:38:46 -0500

    added date text and streak counter text

--- app/src/main/java/com/hacktj/studystudio/MainActivity.kt
+++ app/src/main/java/com/hacktj/studystudio/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hacktj.studystudio
 
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var calendar: Calendar
+    private lateinit var streakText: TextView
+    private lateinit var dateText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calendar = Calendar.getInstance()
+        dateText = findViewById(R.id.dateView)
+        streakText = findViewById(R.id.streakText)
+        dateText.text = getDate()
+        streakText.text = getString(R.string.streak, 0)
     }
+    private fun getDate(): String {
+        val month = calendar.get(Calendar.MONTH) + 1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val year = calendar.get(Calendar.YEAR)
+        return "$month/$day/$year"
+    }
+
 }
