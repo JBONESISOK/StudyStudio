7bea116ca01998c3116c29c8a5ea0c9571bd1490
badumyz <hoben164@hotmail.com>
2023-03-05 00:21:13 -0500

    timer is done

--- app/src/main/java/com/hacktj/studystudio/MainActivity.kt
+++ app/src/main/java/com/hacktj/studystudio/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hacktj.studystudio
 import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +13,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       setContentView(R.layout.activity_main)
+        setContentView(R.layout.survey)
+        /*
         calendar = Calendar.getInstance()
         dateText = findViewById(R.id.dateView)
         streakText = findViewById(R.id.streakText)
         dateText.text = getDate()
         streakText.text = getString(R.string.streak, 0)
+        */
+
+
 
     }
     private fun getDate(): String {
---
+++ app/src/main/java/com/hacktj/studystudio/SurveyActivity2.kt
@@ -0,0 +1,27 @@
+package com.hacktj.studystudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveyActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.survey)
+        var num = 0
+        button.setOnClickListener {
+
+            num++
+            println(num)
+            Number.text = num.toString()
+        }
+        buttonDecrement.setOnClickListener {
+            if (num > 0) {
+                num--
+                println("testing")
+                Number.text = num.toString()
+            }
+            println(num)
+        }
+    }
+    }
+}
