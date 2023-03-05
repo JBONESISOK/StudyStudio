03ac70719bb3bf09d129f7ba48e7e5afe6b05bf6
Luan <bambuluan@gmail.com>
2023-03-04 23:24:26 -0500

    Luan changes

--- app/src/main/java/com/hacktj/studystudio/MainActivity.kt
+++ app/src/main/java/com/hacktj/studystudio/MainActivity.kt
@@ -1,44 +1,25 @@
 package com.hacktj.studystudio
-
+import androidx.appcompat.app.AppCompatActivity as w
+import android.os.Bundle as B
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        var randomNum = (1 until 11).random()
-        randomNum = randomNum.toInt()
-        var randomText : String = ""
-        if(randomNum == 1){
-            randomText = "Good sleep is essential for learning."
-        }
-        else if(randomNum == 2) {
-            randomText = "Mistakes are part of how you learn."
-        }
-        else if(randomNum == 3){
-            randomText = "Listening to music can improve concentration and motivation to study!"
-        }
-        else if(randomNum == 4){
-            randomText = "Remember to take frequent breaks."
-        }
-        else if(randomNum == 5){
-            randomText = "Using colors are a great way to organize!"
-        }
-        else if(randomNum == 6){
-            randomText = "Study smarter, not harder!"
-        }
-        else if(randomNum == 7){
-            randomText = "Study ahead, don't want until the deadline."
-        }
-        else if(randomNum == 8){
-            randomText = "Study groups can help avoid procrastination!"
-        }
-        else if(randomNum == 9){
-            randomText = "Reward yourself, it makes studying much more motivating!"
-        }
-        else if(randomNum == 10){
-            randomText = "Setting small goals for yourself is a great way to progress!"
-        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get reference to the string array that we just created
+        val languages = resources.getStringArray(R.array.programming_languages)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown, languages)
+        // get reference to the autocomplete text view
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        // set adapter to the autocomplete tv to the arrayAdapter
+        autocompleteTV.setAdapter(arrayAdapter)
+
     }
 
 }
