afbfccac3b75e589d75f24aa67fc409495cd5c89
Jalil <68628169+JBONESISOK@users.noreply.github.com>
2023-03-05 03:07:02 -0500

    added working timer with concurrency and quotes

--- app/src/main/java/com/hacktj/studystudio/Home.kt
+++ app/src/main/java/com/hacktj/studystudio/Home.kt
@@ -19,15 +19,9 @@ class Home : Fragment() {
     private lateinit var calendar: Calendar
     private lateinit var streakText: TextView
     private lateinit var dateText: TextView
-    private lateinit var quoteText: TextView
-    private val client = HttpClient(Android) {
-        install(ContentNegotiation) {
-            json()
-        }
+    var americanFormat = true
 
-    }
     private val scope = MainScope()
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,10 +29,6 @@ class Home : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         calendar = Calendar.getInstance()
         dateText = view.findViewById(R.id.dateText)
-        quoteText = view.findViewById(R.id.quoteText)
-        scope.launch {
-            getQuote()
-        }
         dateText.text = getDate()
         streakText = view.findViewById(R.id.streakText)
         streakText.text = getString(R.string.streak, 0)
@@ -50,22 +40,11 @@ class Home : Fragment() {
         val month = calendar.get(Calendar.MONTH) + 1
         val day = calendar.get(Calendar.DAY_OF_MONTH)
         val year = calendar.get(Calendar.YEAR)
-        return "$month/$day/$year"
-    }
-
-    private final val API_KEY = "3WRAPtLvwlpapkIxJgm7hA==Bx1tm3Q38VGeckS6"
-    private final val API_LINK = "https://api.api-ninjas.com/v1/quotes?category=inspirational"
-    private suspend fun getQuote(){
-        // Find api and grab quote
-        try {
-            val quote = client.get(API_LINK) {
-                headers["X-API-KEY"] = API_KEY
-            }
-            println(quote.status)
-        } catch(e: java.lang.Exception) {
-            println(e)
+        return if(americanFormat) {
+            "$month/$day/$year"
+        } else {
+            "$day/$month/$year"
         }
     }
-
 
 }
--- app/src/main/java/com/hacktj/studystudio/Timer.kt
+++ app/src/main/java/com/hacktj/studystudio/Timer.kt
@@ -5,55 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.w3c.dom.Text
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Timer.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Timer : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var quoteText: TextView
+    private val scope = MainScope()
+    private var minutes: Int = 0
+    private var seconds: Int = 0
+    private lateinit var addButton: Button
+    private lateinit var decrementButton: Button
+    private lateinit var startButton: Button
+    private lateinit var minutesText: TextView
+    private lateinit var secondsText: TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
+        val view = inflater.inflate(R.layout.fragment_timer, container, false)
+        val colonText: TextView = view.findViewById(R.id.colonText)
+        colonText.text = ":"
+        minutesText = view.findViewById(R.id.minutesText)
+        secondsText = view.findViewById(R.id.secondsText)
+        minutesText.text = "00"
+        secondsText.text = "00"
+        quoteText = view.findViewById(R.id.quoteView)
+        quoteText.text = motivationalQuotes.random()
+        addButton = view.findViewById(R.id.incrementMinutes)
+        addButton.setOnClickListener {
+            minutes += 10
+            minutesText.text = minutes.toString()
+        }
+        decrementButton = view.findViewById(R.id.decrementMinutes)
+        decrementButton.setOnClickListener {
+            if (minutes >= 10) {
+                minutes -= 10
+                minutesText.text = minutes.toString()
+            }
+        }
+        startButton = view.findViewById(R.id.startButton)
+        startButton.setOnClickListener {
+            startTimer()
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Timer.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Timer().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun startTimer() {
+        scope.launch {
+            while (minutes != 0 || seconds != 0) {
+                delay(1000)
+                if (seconds == 0) {
+                    seconds = 60
+                    minutes--
                 }
+                println(seconds)
+                seconds--
+                secondsText.text = seconds.toString()
+                minutesText.text = minutes.toString()
             }
+        }
     }
+
 }
