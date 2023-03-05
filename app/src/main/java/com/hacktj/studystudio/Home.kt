1e7f1ac86fcbe0922850dbf377b31d0b210c89a0
Jalil <68628169+JBONESISOK@users.noreply.github.com>
2023-03-04 23:18:23 -0500

    new changes

--- app/src/main/java/com/hacktj/studystudio/Home.kt
+++ app/src/main/java/com/hacktj/studystudio/Home.kt
@@ -2,28 +2,43 @@ package com.hacktj.studystudio
 
 import android.icu.util.Calendar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.hacktj.studystudio.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.*
 
 
 class Home : Fragment() {
     private lateinit var calendar: Calendar
     private lateinit var streakText: TextView
     private lateinit var dateText: TextView
+    private lateinit var quoteText: TextView
+    private val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json()
+        }
 
+    }
+    private val scope = MainScope()
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         calendar = Calendar.getInstance()
         dateText = view.findViewById(R.id.dateText)
+        quoteText = view.findViewById(R.id.quoteText)
+        scope.launch {
+            getQuote()
+        }
         dateText.text = getDate()
         streakText = view.findViewById(R.id.streakText)
         streakText.text = getString(R.string.streak, 0)
@@ -38,8 +53,18 @@ class Home : Fragment() {
         return "$month/$day/$year"
     }
 
-    private fun getQuote() {
+    private final val API_KEY = "3WRAPtLvwlpapkIxJgm7hA==Bx1tm3Q38VGeckS6"
+    private final val API_LINK = "https://api.api-ninjas.com/v1/quotes?category=inspirational"
+    private suspend fun getQuote(){
         // Find api and grab quote
+        try {
+            val quote = client.get(API_LINK) {
+                headers["X-API-KEY"] = API_KEY
+            }
+            println(quote.status)
+        } catch(e: java.lang.Exception) {
+            println(e)
+        }
     }
 
 
